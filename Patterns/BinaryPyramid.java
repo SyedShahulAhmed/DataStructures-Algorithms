1c6f6993f80d31d3dafa4ba2c27f549ac8b5d9a5
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-01-14 08:00:34 +0530

    0-1 Binary Triangle

---
+++ Patterns/BinaryPyramid.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+class BinaryPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <=i; j++){
+                int sum = i + j;
+                if( sum %2 == 0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
