2a3b7293e38bff985af328f8b15dbdae005a7701
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-01-20 20:49:21 +0530

    Numbers's Pyramid

---
+++ Patterns/NumberPyramid.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+class NumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++ ){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= i; k++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+       
+        sc.close();
+    }
+}
