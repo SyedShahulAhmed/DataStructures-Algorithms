fcad33f80c811bfcde2cf2bf14736c11efb6b428
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-01-27 21:50:52 +0530

    Infinite do-while Loop

---
+++ Basics/ExerciseProblems/Problem6.java
@@ -0,0 +1,9 @@
+package ExerciseProblems;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        do{
+            System.out.println(1);
+        }while( 1 > 0);
+    }
+}
