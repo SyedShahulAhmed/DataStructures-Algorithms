742e1dec8f1ad8e0998793a1782d6fdb26fdd52e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-02-08 18:09:13 +0530

    Strictly Increasing Sorting

--- Recursion/Recursion2/StrictSorting.java
+++ Recursion/Recursion2/StrictSorting.java
@@ -1,16 +1,14 @@
 package Recursion.Recursion2;
 
 public class StrictSorting {
-    // public static boolean one(int[] arr){
-    //     for(int i = 0; i < arr.length; i++){
-    //         for(int j = i+1; j < arr.length; j++){
-    //             if(arr[i] >= arr[j]){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
+    public static boolean one(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+                if(arr[i] >= arr[i+1]){
+                    return false;
+                }
+        }
+        return true;
+    }
     public static boolean StrictSort(int arr[],int idx){
         if(idx == arr.length-1){
             return true;
@@ -23,8 +21,9 @@ public class StrictSorting {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,4,5,6};
+        int arr[] = {1,2,3,4,5,6};
         boolean res = StrictSort(arr,0);
         System.out.println(res);
+        System.out.println(one(arr));
     }
 }
