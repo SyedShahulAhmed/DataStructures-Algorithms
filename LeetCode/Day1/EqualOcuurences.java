c624f7c65a1ff9bf15aa3742a35cfffdceb8b81d
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-02-17 21:35:29 +0530

    Passed all Testcases

--- LeetCode/Day1/EqualOcuurences.java
+++ LeetCode/Day1/EqualOcuurences.java
@@ -1,23 +1,29 @@
 package LeetCode.Day1;
 
+import java.util.Arrays;
+
 public class EqualOcuurences {
-    public static boolean Occurences(String s1){
-        char c[] = s1.toCharArray();
-        int count = 0;
-        for(int i = 0; i < c.length-1 ; i++){
-            if(c[i] == c[i+1]){
+    public static boolean equalOccurrences(String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        System.out.println(c);
+        int count = 1, prev = 0;
+        
+        for (int i = 1; i < c.length; i++) {
+            if (c[i] == c[i - 1]) {
                 count++;
-            }else{
-                count--;
+            } else {
+                if (prev != 0 && prev != count) {
+                    return false;
+                }
+                prev = count;
+                count = 1;
             }
         }
-        if(count %2 != 0){
-            return true;
-        }
-        return false;
+        return true;
     }
-    public static void main(String[] args) {
-        String s = "abacbc";
-        System.out.println(Occurences(s));
+       public static void main(String[] args) {
+        String s = "aaabb";
+        System.out.println(equalOccurrences(s));
     }
 }
